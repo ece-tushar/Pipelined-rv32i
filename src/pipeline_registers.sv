901b116d99b55a327e96646d7198b4cb7074bac1
ece-tushar <indigotushar@gmail.com>
2026-07-02 16:07:14 +0530

    Update pipeline_registers.sv - dropped control word, pipelined the Instruction

--- src/pipeline_registers.sv
+++ src/pipeline_registers.sv
@@ -53,14 +53,14 @@ module pipe_ID_EX #(
        input logic [DATA_WIDTH-1:0] Rs1_in,
        input logic [DATA_WIDTH-1:0] Rs2_in,
        input logic [DATA_WIDTH-1:0] Imm_in,
-       input logic [CTRL_WORD_WIDTH-1:0] Ctrl_in,
+       input logic [DATA_WIDTH-1:0] IM_in,
         
        output logic [ADDR_WIDTH-1:0] PC4_out,
        output logic [REG_ADDR_WIDTH-1:0] Rd_addr_out,
        output logic [DATA_WIDTH-1:0] Rs1_out,
        output logic [DATA_WIDTH-1:0] Rs2_out,
        output logic [DATA_WIDTH-1:0] Imm_out,
-       output logic [CTRL_WORD_WIDTH-1:0] Ctrl_out
+       output logic [DATA_WIDTH-1:0] IM_out
        );
        
    struct packed {
@@ -69,7 +69,7 @@ module pipe_ID_EX #(
         logic [DATA_WIDTH-1:0] RS1;
         logic [DATA_WIDTH-1:0] RS2;
         logic [DATA_WIDTH-1:0] IMM;
-        logic [CTRL_WORD_WIDTH-1:0] CTRL;
+        logic [DATA_WIDTH-1:0] IM;
         } ID_EX;
    
    always_ff @ (posedge Clk) begin
@@ -83,7 +83,7 @@ module pipe_ID_EX #(
             ID_EX.RS1   <= Rs1_in;
             ID_EX.RS2   <= Rs2_in;
             ID_EX.IMM   <= Imm_in;
-            ID_EX.CTRL  <= Ctrl_in;
+            ID_EX.IM  <= IM_in;
             end
    end
 
@@ -92,7 +92,7 @@ assign Rd_addr_out = ID_EX.RD;
 assign Rs1_out = ID_EX.RS1;             
 assign Rs2_out = ID_EX.RS2;             
 assign Imm_out = ID_EX.IMM;             
-assign Ctrl_out = ID_EX.CTRL;             
+assign IM_out = ID_EX.IM;             
       
 endmodule
 
@@ -109,13 +109,13 @@ module pipe_EX_MEM #(
        input logic [REG_ADDR_WIDTH-1:0] Rd_addr_in,
        input logic [DATA_WIDTH-1:0] Rs2_in,
        input logic [DATA_WIDTH-1:0] ALU_in,
-       input logic [CTRL_WORD_WIDTH-1:0] Ctrl_in,
+       input logic [DATA_WIDTH-1:0] IM_in,
         
        output logic [ADDR_WIDTH-1:0] PC4_out,
        output logic [REG_ADDR_WIDTH-1:0] Rd_addr_out,
        output logic [DATA_WIDTH-1:0] Rs2_out,
        output logic [DATA_WIDTH-1:0] ALU_out,
-       output logic [CTRL_WORD_WIDTH-1:0] Ctrl_out
+       output logic [DATA_WIDTH-1:0] IM_out
        );
        
    struct packed {
@@ -123,7 +123,7 @@ module pipe_EX_MEM #(
         logic [REG_ADDR_WIDTH-1:0] RD;
         logic [DATA_WIDTH-1:0] RS2;
         logic [DATA_WIDTH-1:0] ALU;
-        logic [CTRL_WORD_WIDTH-1:0] CTRL;
+        logic [DATA_WIDTH-1:0] IM;
         } EX_MEM;
    
    always_ff @ (posedge Clk) begin
@@ -136,7 +136,7 @@ module pipe_EX_MEM #(
             EX_MEM.RD    <= Rd_addr_in;
             EX_MEM.RS2    <= Rs2_in;
             EX_MEM.ALU   <= ALU_in;
-            EX_MEM.CTRL  <= Ctrl_in;
+            EX_MEM.IM  <= IM_in;
             end
    end
 
@@ -144,7 +144,7 @@ assign PC4_out = EX_MEM.PC4;
 assign Rd_addr_out = EX_MEM.RD;             
 assign Rs2_out = EX_MEM.RS2;             
 assign ALU_out = EX_MEM.ALU;             
-assign Ctrl_out = EX_MEM.CTRL;             
+assign IM_out = EX_MEM.IM;             
       
 endmodule
 
@@ -160,13 +160,13 @@ module pipe_MEM_WB #(
        input logic [REG_ADDR_WIDTH-1:0] Rd_addr_in,
        input logic [DATA_WIDTH-1:0] ALU_in,
        input logic [DATA_WIDTH-1:0] DM_in,
-       input logic [CTRL_WORD_WIDTH-1:0] Ctrl_in,
+       input logic [DATA_WIDTH-1:0] IM_in,
         
        output logic [ADDR_WIDTH-1:0] PC4_out,
        output logic [REG_ADDR_WIDTH-1:0] Rd_addr_out,
        output logic [DATA_WIDTH-1:0] ALU_out,
        output logic [DATA_WIDTH-1:0] DM_out,
-       output logic [CTRL_WORD_WIDTH-1:0] Ctrl_out
+       output logic [DATA_WIDTH-1:0] IM_out
        );
        
    struct packed {
@@ -174,7 +174,7 @@ module pipe_MEM_WB #(
         logic [REG_ADDR_WIDTH-1:0] RD;
         logic [DATA_WIDTH-1:0] DM;
         logic [DATA_WIDTH-1:0] ALU;
-        logic [CTRL_WORD_WIDTH-1:0] CTRL;
+        logic [DATA_WIDTH-1:0] IM;
         } MEM_WB;
    
    always_ff @ (posedge Clk) begin
@@ -187,7 +187,7 @@ module pipe_MEM_WB #(
             MEM_WB.RD    <= Rd_addr_in;
             MEM_WB.DM    <= DM_in;
             MEM_WB.ALU   <= ALU_in;
-            MEM_WB.CTRL  <= Ctrl_in;
+            MEM_WB.IM  <= IM_in;
             end
    end
 
@@ -195,6 +195,6 @@ assign PC4_out = MEM_WB.PC4;
 assign Rd_addr_out = MEM_WB.RD;             
 assign DM_out = MEM_WB.DM;             
 assign ALU_out = MEM_WB.ALU;             
-assign Ctrl_out = MEM_WB.CTRL;             
+assign IM_out = MEM_WB.IM;             
       
 endmodule
